a1e76f58789d909e1530620a02ea1429f9fce0c7
kushwahramkumar2003 <kushwahramkumar2003@gmail.com>
2024-03-09 23:53:36 +0530

    categories field created

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -6,12 +6,14 @@ import packageJSON from "../package.json";
 import constants from "@/constants";
 import ImageScroller from "@/components/core/home/ImageScroller";
 import TodaySale from "@/components/core/home/TodaySale";
+import Categories from "@/components/core/home/Categories";
 
 export default function Index() {
   return (
     <div className="space-y-10 w-full">
       <ImageScroller images={constants.images.iphoneImgArr} />
       <TodaySale />
+      <Categories />
     </div>
   );
 }
