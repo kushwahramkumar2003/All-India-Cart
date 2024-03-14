07c320014387425720434a4a3badca07b09e5ffc
kushwahramkumar2003 <kushwahramkumar2003@gmail.com>
2024-03-14 21:09:06 +0530

    bread crumbs component added

---
+++ apps/web/app/contact/page.tsx
@@ -0,0 +1,12 @@
+export default function Index() {
+    return (
+        <div className="w-full space-y-10 transition-all duration-300 mt-4 mb-10">
+        <div>
+
+        </div>
+        <div></div>
+        </div>
+    );
+}
+
+
