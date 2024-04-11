1b95237a42b84b8398eaeb0cc058135f3bbdf498
kushwahramkumar2003 <kushwahramkumar2003@gmail.com>
2024-04-11 18:27:45 +0530

    routes added

--- apps/api/src/routes/auth.routes.ts
+++ apps/api/src/routes/auth.routes.ts
@@ -1,5 +1,11 @@
 import { Router } from 'express'
+import { logout, sellerLogin, sellerRegester, updatePassword } from '../controllers/auth.controllers'
 
 const router = Router()
+
+router.post('/seller/regester', sellerRegester)
+router.post('/seller/login', sellerLogin)
+router.get('/logout', logout)
+router.put('/seller/updatePassword', updatePassword)
 
 export default router
--- apps/api/src/routes/index.routes.ts
+++ apps/api/src/routes/index.routes.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express'
 import authRoutes from './auth.routes'
+import categoryRoutes from './category.routes'
+import couponRoutes from './coupon.routes'
 import { rateLimitMiddleware } from '../middlewares/rateLimitter'
 
 const router = Router()
 
 router.use('/auth', rateLimitMiddleware, authRoutes)
+router.use('/category', rateLimitMiddleware, categoryRoutes)
+router.use('/coupon', rateLimitMiddleware, couponRoutes)
 
 export default router
