6a29dc692f9c5b7a9094098f26f25929a68c6d13
kushwahramkumar2003 <kushwahramkumar2003@gmail.com>
2024-04-16 16:36:22 +0530

    create new product page completed

---
+++ apps/admin/src/services/index.ts
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+import config from '@/config/index';
+
+axios.defaults.baseURL = config.baseUrl;
+axios.defaults.withCredentials = true;
+
+const axiosClient = axios.create({
+  baseURL: config.baseUrl,
+  withCredentials: true,
+});
+
+export default axiosClient;
---
+++ apps/admin/src/services/product.ts
@@ -0,0 +1,37 @@
+import axiosClient from '@/services/index';
+import axios, { AxiosError } from 'axios';
+import { z } from 'zod';
+
+import { ProductSchema } from '@/components/dashboard/products/new/new-product-form';
+
+// export const createNewProduct = async (data: z.infer<typeof ProductSchema>) => {
+//   try {
+//     console.log('Axios call data --> ', data);
+//     const response = await axiosClient.post('/product', data);
+//     return response.data;
+//   } catch (error: unknown) {
+//     if (axios.isAxiosError(error)) {
+//       console.log('Axios error:', error.message);
+//       console.log('Axios error:', error);
+//
+//       if (error.response?.data) {
+//         throw new Error(error.response?.data.message);
+//       }
+//       throw new Error(error.message);
+//     }
+//     console.log('Unknown error:', error);
+//   }
+// };
+
+export function createNewProduct(formData: z.infer<typeof ProductSchema>) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const { data } = await axiosClient.post('/product', formData);
+      console.log('data :>> ', data);
+      resolve(data);
+    } catch (error) {
+      console.error('Error in create new product function:', error);
+      reject(error);
+    }
+  });
+}
