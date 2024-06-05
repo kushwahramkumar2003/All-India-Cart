e96f7439e399fd163695214bbfc4f03dd3870d44
kushwahramkumar2003 <kushwahramkumar2003@gmail.com>
2024-06-06 00:16:50 +0530

    cart controller added

--- apps/api/src/utils/asyncHandler.ts
+++ apps/api/src/utils/asyncHandler.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from 'express'
-import { ZodError } from 'zod'
+import z, { ZodError } from 'zod'
 
 type AsyncHandler = (req: Request, res: Response, next: NextFunction) => Promise<void> | void | Promise<string>
 
@@ -7,14 +7,12 @@ const asyncHandler = (fn: AsyncHandler) => async (req: Request, res: Response, n
   try {
     await fn(req, res, next)
   } catch (error) {
-    if (error instanceof ZodError) {
-      console.log('error : ', error)
-      return res.status(400).json({ message: error.message })
+    if (error instanceof z.ZodError) {
+      res.status(400).json({ error: error.errors })
     }
-
     if (error instanceof Error) {
       console.log('error : ', error)
-      return res.status(400).json({ message: error.message })
+      return res.status(400).json({ error: error.message })
     }
   }
 }
