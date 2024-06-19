5e4e37e3ac3face4a7f6aed74d778f29aa041cb7
kushwahramkumar2003 <kushwahramkumar2003@gmail.com>
2024-06-19 15:31:49 +0530

    docker file added to CD

--- apps/api/src/utils/redisClient.ts
+++ apps/api/src/utils/redisClient.ts
@@ -2,11 +2,15 @@ import { createClient } from 'redis'
 import config from '../config'
 
 const redisClient = createClient({
-  password: config.REDIS_PASSWORD,
-  socket: {
-    host: config.REDIS_HOST,
-    port: Number(config.REDIS_PORT) || 6379
-  }
+  url: config.REDIS_URL,
+  pingInterval: 1000 * 60 * 1
 })
+// const redisClient = createClient({
+//   password: config.REDIS_PASSWORD,
+//   socket: {
+//     host: config.REDIS_HOST,
+//     port: Number(config.REDIS_PORT) || 17911
+//   }
+// })
 
 export default redisClient
